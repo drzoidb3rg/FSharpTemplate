e334cdf8c8811a70fe869bcbb412a7f4e070cbc1
martin bailey <martin.bailey@gmail.com>
2015-09-18 10:47:57 +0100

    test

--- tests/HDAS.Tests/Script.fs
+++ tests/HDAS.Tests/Script.fs
@@ -1,14 +1,9 @@
-#I "../../bin/HDASSearchService"
-#I "../../src/HDASSearchService/bin/"
-#I "../HDASSearchService.Tests/bin/"
-#r "../../packages/Fuchu/lib/Fuchu.dll"
-#r "../../packages/Suave/lib/net40/Suave.dll"
-#r "../../packages/FsPickler/lib/net40/FsPickler.dll"
-#r "../../packages/Suave.Testing/lib/net40/Suave.Testing.dll"
-#r @"System.Net.Http"
-#r "../../packages/FSharp.Data/lib/net40/FSharp.Data.dll"
-#r "../../packages/Unquote/lib/net40/Unquote.dll"
-#r "../../packages/NUnit/lib/nunit.framework.dll"
+#I "bin/Release"
+#r "bin/Release/nunit.framework.dll"
+#r "bin/Release/Suave.dll"
+#r "bin/Release/Suave.Testing.dll"
+#r "bin/Release/HDAS.Tests.dll"
+#r "bin/Release/Fuchu.dll"
 
 open Suave
 open Suave.Http
@@ -17,10 +12,7 @@ open Suave.Http.Successful
 open Suave.Web
 open Suave.Types
 open NUnit.Framework
-open Swensen.Unquote
-open Swensen.Unquote.Operators
 open Suave.Testing
-open Fuchu
 
 
 //Set pwd to the directory containing this script
@@ -35,7 +27,14 @@ let hdasApp =
 let defaultConfig = { defaultConfig with
                                     bindings = [ HttpBinding.mk' HTTP "0.0.0.0" 8083 ]
                                     homeFolder = Some (__SOURCE_DIRECTORY__ + "/web")
-                    }
+                      }
 
-//printf "Running with config:\n%A" defaultConfig
-//startWebServer defaultConfig hdasApp
+[<Test>]
+let ``Should do home`` () =
+  let home =
+    startServer ()
+    |> req HttpMethod.GET "/" None
+    ()
+  test <@ title = "KB - Home" @>
+
+``Should do home`` () 
